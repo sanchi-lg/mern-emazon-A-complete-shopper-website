75c2460630cc0731aa8e848557603a5b51bf4ea0
sanchi <goelsanchi2000@gmail.com>
2020-12-01 01:20:47 +0530

    dotcomm

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config({ path: './d.env' })
+// require('dotenv').config({ path: './d.env' })
 
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
