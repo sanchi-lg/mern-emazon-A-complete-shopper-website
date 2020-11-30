eec4cd3ab3c9415ecc0bfe94c2ca954c88465fbe
sanchi <goelsanchi2000@gmail.com>
2020-12-01 00:43:21 +0530

    coloncomm

--- keys.js
+++ keys.js
@@ -2,7 +2,7 @@
 if(process.env.NODE_ENV=="production"){
     module.exports={
         MONGO_URL:process.env.MONGO_URL,
-        MONGODB_URL:rocess.env.MONGODB_URL,
+        MONGODB_URL:process.env.MONGODB_URL,
         EMAIL:process.env.EMAIL,
         PASSWORD:process.env.PASSWORD,
         RPORT:process.env.RPORT
