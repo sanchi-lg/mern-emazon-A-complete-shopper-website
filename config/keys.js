80e2dbee37885f6ff63d5256fee8cc7c9bc93f80
sanchi <goelsanchi2000@gmail.com>
2020-12-01 12:38:23 +0530

    configcommit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+if(process.env.NODE_ENV=='production'){
+    module.exports={
+        MONGO_URL:process.env.MONGO_URL,
+        MONGODB_URL:process.env.MONGODB_URL,
+        EMAIL:process.env.EMAIL,
+        PASSWORD:process.env.PASSWORD,
+        RPORT:process.env.RPORT
+
+
+    }
+}
+else{
+    module.exports=require('./env')
+}
