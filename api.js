a3e9ecf6f5780cb98fd78836e07abb290876e4c0
sanchi <goelsanchi2000@gmail.com>
2020-12-01 19:38:43 +0530

    newpack

--- api.js
+++ api.js
@@ -40,10 +40,10 @@ let storage = multer.diskStorage({
 let uploa = multer({ storage: storage }).single('attach')
 app.use(express.static('upload'))
 if(process.env.NODE_ENV=="production"){
-app.use(express.static('frontend/build'))
+app.use(express.static('front/build'))
 const path=require('path')
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'front','build','index.html'))
 })
 }
 
