59c42ae098ceb0f4bd31b3ed04f54eeb1ac607c3
sanchi <goelsanchi2000@gmail.com>
2020-12-03 03:05:14 +0530

    lastcommit

--- front/src/components/ser.js
+++ front/src/components/ser.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const murl=""
 
 export function productsdis() {
-    return axios.get("${murl}/productsdis")
+    return axios.get(`${murl}/productsdis`)
 }
 export function productsdisbyid(v) {
     return axios.get(`${murl}/productsdis/${v}`)
