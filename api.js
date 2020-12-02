e41792610141b17b16d00276ee48f26bb1ae99b1
sanchi <goelsanchi2000@gmail.com>
2020-12-02 10:20:10 +0530

    dircomm

--- api.js
+++ api.js
@@ -69,7 +69,7 @@ let storage = multer.diskStorage({
 let uploa = multer({ storage: storage }).single('attach')
 app.use(express.static('upload'))
 if(process.env.NODE_ENV=="production"){
-app.use(express.static('front/build'))
+app.use(express.static(__dirname,'front','build'))
 const path=require('path')
 app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'front','build','index.html'))
@@ -124,7 +124,7 @@ app.get("/productsdis/:v?", (req, res) => {
         productModel.find({}, (err, docs) => {
             console.log("/////////////////////productsdis////////////////////////////////////////////////////");
             var ob = docs;
-            console.log();
+            console.log(ob);
             res.json({ "ob": ob });
 
 
